125a3ed04806be402dba0bcb05110613305e2315
Sava Barbarov <45712571+savabarbarov96@users.noreply.github.com>
2023-03-24 15:03:30 +0200

    Update to app.py
    Added error handling

--- app.py
+++ app.py
@@ -1,21 +1,33 @@
+import re
 from flask import Flask, render_template, request
 from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
 def week_to_date(week_string):
-    if week_string.startswith("wk"):
-        week_string = week_string[2:]
+    try:
+        if week_string.startswith("wk"):
+            week_string = week_string[2:]
 
-    year = int(week_string[:2]) + 2000
-    week = int(week_string[2:4])
-    weekday = int(week_string[5])
+        pattern = re.compile(r'^\d{4}\.\d$')
+        if not pattern.match(week_string):
+            return "Error: Invalid input format. Please use 'wkXXXX.X' or 'XXXX.X"
 
-    first_day = datetime(year, 1, 1)
-    first_monday = first_day + timedelta(days=(1 - first_day.isoweekday()) % 7)
-    target_date = first_monday + timedelta(weeks=(week - 1), days=(weekday - 1))
+        year = int(week_string[:2]) + 2000
+        week = int(week_string[2:4])
+        weekday = int(week_string[5])
 
-    return target_date.strftime('%d %B, %A')
+        if week < 1 or week > 53 or weekday < 1 or weekday > 7:
+            return "Error: Invalid week or weekday value. Week should be between 1 and 53, and weekday should be between 1 and 7."
+
+        first_day = datetime(year, 1, 1)
+        first_monday = first_day + timedelta(days=(1 - first_day.isoweekday()) % 7)
+        target_date = first_monday + timedelta(weeks=(week - 1), days=(weekday - 1))
+
+        return target_date.strftime('%d %B, %A')
+    except Exception as e:
+        return f"Error: An unexpected error occurred: {str(e)}"
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
